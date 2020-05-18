cb593fa16f9d562da954bf29ca00a9c528239d38
Kathryn Rhoads <kathryn.ann.rhoads@gmail.com>
2020-05-18 18:42:13 -0400

    enforced types, more permission context

--- src/Permissions.tsx
+++ src/Permissions.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
-
-const PermissionsContext = React.createContext();
-
-const PermissionProvider = ({children}) => {
+import React, {useState} from 'react';
+type Permission = 'user:write' | 'user:read' | 'user:admin';
+type PermissionContextValue = {
+    permissions: Permission[],
+}
+const PermissionsContext = React.createContext<PermissionContextValue | null>(null);
+       //typed as a react componennt that expects a children prop
+export const PermissionsProvider: React.FC = ({children}) => {
+   const [permissions, setPermissions] = useState<Permission[]>(['user:write', 'user:read'])
     return (
-        <PermissionsContext.Provider value={undefined}>
+        <PermissionsContext.Provider value={{permissions}}>
          {children}
         </PermissionsContext.Provider>
     )
--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 //context provider
 import { BrowserRouter as Router } from 'react-router-dom';
+import { PermissionsProvider} from './Permissions'
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <PermissionsProvider>
+        <App />
+      </PermissionsProvider> 
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
